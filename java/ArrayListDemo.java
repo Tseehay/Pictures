d00009f05e9551ce46bb82ab9536ede6c4082b1f
Tsehay Goremes <ttsehay499@gmail.com>
2023-12-12 11:05:04 +0300

    create arrayList

--- java/ArrayListDemo.java
+++ java/ArrayListDemo.java
@@ -1,3 +1,14 @@
+import java.util.*;
+
 public class ArrayListDemo {
-    
+    public static void main(String[] args) {
+        ArrayList<String> al= new ArrayList<String>();
+        al.add("Addis");
+        al.add("Adama");
+        al.add("Debrezeyt");
+        al.add("Gonder");
+        al.add("Harer");
+        System.out.println("size of array list: "+al.size()); 
+          
+    }
 }
