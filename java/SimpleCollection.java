529a95cde691f93cb156f8cb3533288d5769b07a
Tsehay Goremes <ttsehay499@gmail.com>
2023-12-12 10:32:15 +0300

    delete

--- java/SimpleCollection.java
+++ java/SimpleCollection.java
@@ -1,12 +1,15 @@
-public class SimpleCollection  {
+import java.util.*;
+
+public class SimpleCollection{
 	public static void main(String[] args) {
 		Collection c;
-		c = new ArrayList();
+		c=new ArrayList();
 		System.out.println(c.getClass().getName());
-		for (int i=1; i <= 10; i++) { 
-			c.add(i + " * " + i + " = "+i*i);
+		for (int i=1;i<=10;i++){
+			c.add(i + " * "+i+" = "+i*i);
 		}
-		Iterator iter = c.iterator();
+		Iterator iter=c.iterator();
 		while (iter.hasNext())
 			System.out.println(iter.next());
 	}
+}
